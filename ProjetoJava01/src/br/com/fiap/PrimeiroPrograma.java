528c9a417f9d09dcba03856fc4838d7efa4b8a7f
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-03-14 12:05:48 -0300

    Add files via upload

---
+++ ProjetoJava01/src/br/com/fiap/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.fiap;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        //Exibindo Hello World em Java!
+        /*
+        Testando
+        Comentários com
+        múltiplas linhas :)
+        */
+        System.out.println("Hello World!");
+    }
+}
